9065fcc3e7dce32ac16020154db691c957021fda
Acifer <50952621+AciferBG@users.noreply.github.com>
2026-07-05 14:27:26 +0200

    update

--- silverrealms/dlg/dialogues_ee/ac#acil63.d
+++ silverrealms/dlg/dialogues_ee/ac#acil63.d
@@ -5,19 +5,8 @@ BEGIN ~AC#DUN02~
 
 IF ~Global("AC#Goapspawn","GLOBAL",7)~ THEN BEGIN hello_find_bresk
 SAY ~Geht und seht nach meinem Sohn Bresk. Er wird als Kriegsmeister außen die Verteidung der Stadt organisieren. Meine Pflicht ist es, hier innen die Stellung zu halten und den König zu beschützen.~
-IF ~~ THEN REPLY ~Wird gemacht.~ + bye_find_bresk
-IF ~~ THEN REPLY ~Ich bin am Ende meiner Kräfte und benötige Heilung.~ + need_healing
+IF ~~ THEN EXIT 
 END
-
-	IF ~~ THEN BEGIN need_healing
-	SAY ~Sagt das doch gleich! Ich kann Euch jederzeit den Segen Gorm Gulthyms schenken!~
-	IF ~~ THEN DO ~StartStore("AC#DUN02",LastTalkedToBy(Myself))~ EXIT 
-	END
-
-	IF ~~ THEN BEGIN bye_find_bresk
-	SAY ~Viel Erfolg, <CHARNAME>!~
-	IF ~~ THEN EXIT 
-	END
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN 1
   SAY ~Gut, dass Ihr hier seid, <CHARNAME>! Die Stadt steht unter Angriff. Habt Ihr meine Söhne gesehen? Was geschieht da draußen?~
@@ -38,7 +27,7 @@ IF ~~ THEN BEGIN 3
 //  ++ ~Ich werde Euch helfen, den König und Iltkazar zu verteidigen.~ + 4
   ++ ~Ihr könnt auf meine Klinge zählen!~ + 4
   ++ ~Ich habe nicht vor, diese Stadt ihrem Schicksal zu überlassen.~ + 4
-  ++ ~Ich kämpfe nicht für Iltkazar. Ich kämpfe für meine eigenen Ziele. Doch heute läuft es auf dasselbe hinaus.~ + 4
+  ++ ~Ich kämpfe nicht für Iltkazar, sondern für meine eigenen Ziele. Heute läuft es auf dasselbe hinaus.~ + 4
 ++ ~Schon wieder eine Stadt retten? Irgendwann sollte man mir dafür eine Statue errichten!~ + 4
 //++ ~Mit Vergnügen! Lasst uns diesen Bestien zeigen, mit wem sie sich angelegt haben.~ + 4
 ++ ~Ich bin nicht den ganzen Weg hierher gekommen, um mich vor einem Kampf zu drücken.~ + 4
@@ -80,30 +69,33 @@ Dialog des Boten nach Goaps Tod
 *******************************************************************************************************/
 BEGIN ~AC#DWMS1~
 
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN 1
-SAY  ~<CHARNAME>! Bresk Steinschulter schickt mich. Es ist noch nicht vorbei. Wir wissen, wo diese Teufel herkommen!~ 
+CHAIN IF ~NumTimesTalkedTo(0)~ THEN AC#DWMS1 1
+~<CHARNAME>! Bresk Steinschulter schickt mich. Es ist noch nicht vorbei. Wir wissen, wo diese Teufel herkommen!~ 
 =
 ~Sie haben die Kuppel der Arnschädelhalle durchbrochen und strömen in Massen aus den unteren Bereichen der Azerkyn-Halle empor!~
+END
 ++ ~Das sind schlechte Nachrichten.~ + 2
 ++ ~Wo ist Bresk jetzt?~ + 2
-END
 
-IF ~~ THEN BEGIN 2
-SAY ~Bresk versucht mit den verbliebenen Kriegern, die Brücke über den Fluss zu halten, doch es sind zu viele Scheusale. Sie werden die Brücke nicht lange halten können!~
+CHAIN AC#DWMS1 2
+~Bresk versucht mit den verbliebenen Kriegern, die Brücke über den Fluss zu halten, doch es sind zu viele Scheusale. Sie werden die Brücke nicht lange halten können!~
 =
 ~Er möchte in die unteren Hallen vordringen und benötigt dabei Eure Hilfe.~
+END
 ++ ~Wir haben hier gerade den Anführer der Unholde erschlagen.~ + 3
 ++ ~Mache mich sogleich auf den Weg.~ + 4
-END
 
-IF ~~ THEN BEGIN 3
-   SAY ~Dieser hier, den Ihr getötet habt? Ich fürchte nicht. Ich bitte Euch, beeilt Euch und schlagt Euch zu Kriegsmeister Bresk vor, sonst wir alle verloren!~
-++ ~Gut, ich werde zur Brücke aufbrechen und nach Bresk schauen.~ + 4
-END
 
-IF ~~ THEN BEGIN 4
-   SAY ~Habt Dank! Ich werde versuchen, in der Stadt weitere versprengte Verteidiger zusammen zu trommeln.~
+CHAIN AC#DWMS1 3
+~Dieser hier, den Ihr getötet habt? Ich fürchte nicht. Ich bitte Euch, beeilt Euch und schlagt Euch zu Kriegsmeister Bresk vor, sonst wir alle verloren!~
+== AC#DUN02 ~Geht und seht nach meinem Sohn Bresk! Meine Pflicht ist es, hier innen die Stellung zu halten und den König zu beschützen.~
+END
+IF ~~ THEN EXTERN ~AC#DWMS1~ 4
+
+CHAIN AC#DWMS1 4
+~Beeilt Euch! Ich werde versuchen, in der Stadt weitere versprengte Verteidiger zusammen zu trommeln.~
+END
    IF ~~ THEN DO ~SetGlobal("AC#Goapspawn","GLOBAL",7)
    EscapeArea()~  EXIT
-END
+
 

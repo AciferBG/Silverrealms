7e1dcb5e7193bf3ec9b6e4273c6d4664a49b9861
Acifer <50952621+AciferBG@users.noreply.github.com>
2026-07-25 13:00:18 +0200

    update

--- silverrealms/dlg/dialogues_ee/ac#acil80.d
+++ silverrealms/dlg/dialogues_ee/ac#acil80.d
@@ -1,24 +1,28 @@
 
-
 /*******************************************************************************************************
-Dialog Cylla-Malphas Cutscene
+Dialog Malphas- Five Claws of Tiamat Cutscene
 *******************************************************************************************************/
-BEGIN ~AC#CYLL1~
+BEGIN AC#MAL03
+BEGIN AC#IL801 // red claw
+BEGIN AC#IL802 // blue claw
+BEGIN AC#IL803 // green claw
+BEGIN AC#IL804 // black claw
+BEGIN AC#IL805 // white claw
 
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN 1
-SAY ~Unsere Feinde sind  an den äußeren Plattformen gelandet, Meister. Soll ich den Wachen befehlen, sie mit unseren Ballisten zu vernichten?~
-IF ~~ THEN EXTERN ~AC#MAL03~ 01
+CHAIN IF ~NumTimesTalkedTo(0)~ THEN AC#IL801 chain_mith_celebration_01
+~Unsere Feinde sind  an den äußeren Plattformen gelandet, Meister. Sollen wir sie mit unseren Ballisten zu vernichten?~
+== AC#MAL03 ~Nein. Lasst sie ruhig kommen. Der psychische Wind ist hier besonders stark. Von diesem Ort gibt es kein Entkommen.~
+== AC#IL802 ~Dann werden wir den Weg zum Tor mit ihrem Blut pflastern.~
+== AC#IL803 ~Unterschätzt sie nicht. Dieser <PRO_RACE> hat bereits mehr erreicht, als ihm je hätte gelingen dürfen.~
+== AC#IL804 ~Es spielt keine Rolle. Wer den Willen der Drachenkönigin herausfordert, wird sterben!~
+== AC#IL805 ~Gebt den Befehl, Meister. Wir werden <PRO_HISHER>en Kopf Tiamat zu Füßen legen.~
+== AC#MAL03 ~Eben deshalb habe ich Euch ausgewählt. Ihr seid die Fünf Klauen Tiamats – ihre stärksten sterblichen Diener. Niemand darf diese Tore passieren.~
+== AC#MAL03 ~Ich ziehe mich nun ins Herz der Festung zurück und bereite die letzte Verteidigung vor. Verschließt die Tore hinter mir. Haltet <CHARNAME> so lange wie möglich auf!~
+== AC#IL801 ~Wie Ihr befehlt, Meister.~
+== AC#MAL03 ~Und wagt es nicht zu versagen. Sollte <CHARNAME> diese Tore durchschreiten, wird Tiamats Zorn das Geringste Eurer Leiden sein.~
+== AC#IL802 ~Dann soll <CHARNAME> kommen. Heute wird eine weitere Seele der Drachenkönigin gehören!~
 END
-
-IF ~~ THEN BEGIN 2
-SAY ~Jawohl, Herr.~
-IF ~~ THEN EXTERN ~AC#MAL03~ 02
-END
-
-IF ~~ THEN BEGIN 3
-SAY ~Ich werde Euch nicht enttäuschen, Meister.~
-IF ~~ THEN DO
-   ~StartCutSceneMode()
+IF ~~ THEN DO~StartCutSceneMode()
    SetGlobal("AC#ACIL80CutScene2","GLOBAL",1)
    ActionOverride("AC#MAL03",EscapeArea())
     Wait(1)
@@ -27,19 +31,6 @@ IF ~~ THEN DO
 	ActionOverride("CUTSPY",DestroySelf())
     Wait(1)
 	MoveViewPoint([267.265],INSTANT)
-    EndCutSceneMode()~EXIT
-END
+    EndCutSceneMode()~EXIT						   
 
-BEGIN ~AC#MAL03~
 
-IF ~~ THEN BEGIN 01
-SAY ~Nein, meine Teuerste. Den Githyanki sind ihre Schiffe heilig, und wir können es uns nicht leisten, unsere Verbündeten derart zu brüskieren. Lasst sie ruhig kommen, unsere Gegner. Der psychische Wind ist hier besonders stark, und von diesem Ort wird es kein Entkommen geben!~ 
-=
-~Ihr habt die ehrenvolle Aufgabe, <CHARNAME> hier vor den Toren unserer Festung zu vernichten.~
-IF ~~ THEN EXTERN ~AC#CYLL1~ 2
-END
-
-IF ~~ THEN BEGIN 02
-SAY ~Ich werde mich ins Innerste der Festung begeben und von dort die weitere Verteidigung vorbereiten. Verschließt die Tore hinter mir und versucht, <CHARNAME> solange es geht aufzuhalten, damit wir Zeit haben, unseren Gefangen von hier fortzubringen. Scheitert nicht an Eurer Aufgabe, denn ansonsten werden Eure Qualen unermesslich sein.~
-IF ~~ THEN EXTERN ~AC#CYLL1~ 3
-END

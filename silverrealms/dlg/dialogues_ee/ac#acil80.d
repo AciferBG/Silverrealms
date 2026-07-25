0f3e2945561d750d1d83fb6d953008d28f53350e
Acifer <50952621+AciferBG@users.noreply.github.com>
2026-07-25 18:09:01 +0200

    update

--- silverrealms/dlg/dialogues_ee/ac#acil80.d
+++ silverrealms/dlg/dialogues_ee/ac#acil80.d
@@ -10,10 +10,10 @@ BEGIN AC#IL804 // black claw
 BEGIN AC#IL805 // white claw
 
 CHAIN IF ~NumTimesTalkedTo(0)~ THEN AC#IL801 chain_mith_celebration_01
-~Unsere Feinde sind  an den äußeren Plattformen gelandet, Meister. Sollen wir sie mit unseren Ballisten zu vernichten?~
+~Unsere Feinde sind  an den äußeren Plattformen gelandet, Meister. Sollen wir sie mit unseren Ballisten vernichten?~
 == AC#MAL03 ~Nein. Lasst sie ruhig kommen. Der psychische Wind ist hier besonders stark. Von diesem Ort gibt es kein Entkommen.~
 == AC#IL802 ~Dann werden wir den Weg zum Tor mit ihrem Blut pflastern.~
-== AC#IL803 ~Unterschätzt sie nicht. Dieser <PRO_RACE> hat bereits mehr erreicht, als ihm je hätte gelingen dürfen.~
+== AC#IL803 ~Unterschätzt <CHARNAME> nicht. Dieser <PRO_RACE> hat bereits mehr erreicht, als <PRO_HIMHER> je hätte gelingen dürfen.~
 == AC#IL804 ~Es spielt keine Rolle. Wer den Willen der Drachenkönigin herausfordert, wird sterben!~
 == AC#IL805 ~Gebt den Befehl, Meister. Wir werden <PRO_HISHER>en Kopf Tiamat zu Füßen legen.~
 == AC#MAL03 ~Eben deshalb habe ich Euch ausgewählt. Ihr seid die Fünf Klauen Tiamats – ihre stärksten sterblichen Diener. Niemand darf diese Tore passieren.~
